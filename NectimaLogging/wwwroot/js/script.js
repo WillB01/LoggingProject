2596d6718da548cd27a802272a93037fc5f268fb
WillB01 <wb.william@outlook.com>
2018-03-16 09:33:43 +0100

    fixed css

---
+++ NectimaLogging/wwwroot/js/script.js
@@ -0,0 +1,14 @@
+﻿
+var el = document.querySelectorAll('.active a');
+console.log(el);
+for (var i = 0; i < el.length; i++) {
+    el[i].addEventListener('click', function (e) {
+        
+        el.style.color = "red";
+      
+    }, false);
+    
+
+}
+
+
